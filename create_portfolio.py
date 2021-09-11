1ee3b7fb2ee63ef76205862af88c800f6514ccc6
Philips <brijesh_deb@yahoo.com>
2021-09-11 13:49:20 +0530

    add banks, networth details

--- create_portfolio.py
+++ create_portfolio.py
@@ -5,15 +5,28 @@ from openpyxl.styles import PatternFill, Font
 # global variables
 vr_mf_start_row=0
 vr_mf_end_row=0
-vr_curr_row=1 #start with 1st row
-vr_mf_cost=0
-vr_mf_value=0
-vr_mf_tot_return=0
-vr_mf_cagr=0
+vr_curr_row=1
+
+dilip_bk_ac = [{"Bank":"ICICI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"SBI-Pension","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"SBI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"Canara","A/C":"xxxxxxxxxxxxxxx","Amount":"0"}]
+
+sobha_bk_ac = [{"Bank":"ICICI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"SBI-Pension","A/C":"xxxxxxxxxxxxxxx","Amount":"0"}]
+
+ratna_bk_ac = [{"Bank":"ICICI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"HDFC","A/C":"xxxxxxxxxxxxxxx","Amount":"0"}]
+
+brijesh_bk_ac = [{"Bank":"ICICI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"ICICI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"Canara","A/C":"xxxxxxxxxxxxxxx","Amount":"0"},
+               {"Bank":"SBI","A/C":"xxxxxxxxxxxxxxx","Amount":"0"}]
 
 prt_curr_row=1
 prt_mf_tot_row = 0
 prt_bank_tot_row= 0
+prt_owner=""
 
 file = openpyxl.load_workbook('my-investment-overviewxls-29-Aug-2021--2025.xltx')
 ws = file.active
@@ -23,73 +36,80 @@ p_ws = p_file.active
 
 # Main function which will orchestrate portfolio file creation
 def create_portfolio():
-    get_vr_cordinate()
-    create_portfolio_header()
-    create_mf_header()
-    create_mf_detail()
-    create_mf_summary()
-    create_bank_header()
-    create_bank_detail()
-    create_bank_summary()
-    create_networth()
-    create_nw_brk_header()
-    create_nw_brk_detail()
-    create_nw_brk_summary()
-    p_file.save("portfolio.xlsx")
+    get_vr_cordinate()      # Get important row numbers from VR file
+    mf_detail_lst= extract_mf_detail(vr_mf_start_row,vr_mf_end_row) # Extract MF details from VR file
+    write_portfolio_header()        # Write Portfolio header
+    write_mf_detail(mf_detail_lst)  # Write MF details in portfolio
+    if prt_owner=="brijesh":
+        write_stock_details()
+    write_bank_detail()             # Write Bank details in Portfolio
+    if prt_owner=="ratna" or prt_owner=="brijesh":
+        write_retiral_detail()
+    if prt_owner=="brijesh":
+        write_realestate_details()
+    write_networth()                # Write Networth in Portfolio
+    write_networth_breakup()        # Write Networth breakup in Portfolio
+    p_file.save("portfolio.xlsx")   # Save Portfolio file in local folder
 
 
-def create_nw_brk_header():
+def write_retiral_detail():
+    pass
+
+def write_stock_details():
+    pass
+
+def write_realestate_details():
+    pass
+
+def write_networth_breakup():
+    add_blank_space()
+    write_header(heading_lst=["Networth Breakup","Amount","VR%"])
+    write_nw_brk_detail()
+
+
+def add_blank_space():
     global prt_curr_row
-    yellowFill = PatternFill('solid', openpyxl.styles.colors.Color(rgb="FFFF66"))
-    prt_curr_row += 1 # skip 1 row
-    p_ws.cell(prt_curr_row,1).value="Networth Breakup"
-    p_ws.cell(prt_curr_row,2).value="Amount"
-    p_ws.cell(prt_curr_row,3).value="VR%"
-    # Change the format for Mutual Fund header
-    for col in range(1,4):
-        p_ws.cell(prt_curr_row,col).fill = yellowFill
-        p_ws.cell(prt_curr_row,col).font = Font(bold=True)
-    prt_curr_row += 1
+    prt_curr_row +=1
 
-def create_nw_brk_detail():
+
+def write_nw_brk_detail():
     global prt_curr_row, prt_bank_tot_row
     # Equity part of MF
     p_ws.cell(prt_curr_row,1).value="Equity - MF"
-    p_ws.cell(prt_curr_row,3).value=0
     p_ws.cell(prt_curr_row,2).value='=($F${0}*$C${1})/100'.format(prt_mf_tot_row,prt_curr_row)
+    p_ws.cell(prt_curr_row,3).value=0
+    mark_input_cell(prt_curr_row,3)
     prt_curr_row += 1
     # Debt part of MF
     p_ws.cell(prt_curr_row,1).value="Debt - MF"
-    p_ws.cell(prt_curr_row,3).value=0
     p_ws.cell(prt_curr_row,2).value='=($F${0}*$C${1})/100'.format(prt_mf_tot_row,prt_curr_row)
+    p_ws.cell(prt_curr_row,3).value=0
+    mark_input_cell(prt_curr_row,3)
     prt_curr_row += 1
     # Cash part of MF
     p_ws.cell(prt_curr_row,1).value="Cash - MF"
-    p_ws.cell(prt_curr_row,3).value=0
     p_ws.cell(prt_curr_row,2).value='=($F${0}*$C${1})/100'.format(prt_mf_tot_row,prt_curr_row)
+    p_ws.cell(prt_curr_row,3).value=0
+    mark_input_cell(prt_curr_row,3)
     prt_curr_row += 1
     # Other part of MF
     p_ws.cell(prt_curr_row,1).value="Other - MF"
-    p_ws.cell(prt_curr_row,3).value=0
     p_ws.cell(prt_curr_row,2).value='=($F${0}*$C${1})/100'.format(prt_mf_tot_row,prt_curr_row)
+    p_ws.cell(prt_curr_row,3).value=0
+    mark_input_cell(prt_curr_row,3)
     prt_curr_row += 1
     # Cash - Bank
     p_ws.cell(prt_curr_row,1).value="Cash - Bank"
-    p_ws.cell(prt_curr_row,3).value=0
     p_ws.cell(prt_curr_row,2).value='=$C{0}'.format(prt_bank_tot_row)
+    p_ws.cell(prt_curr_row,3).value="NA"
     prt_curr_row += 1
 
 
-def create_nw_brk_summary():
-    pass
-
-def create_networth():
+def write_networth():
     global prt_curr_row
     yellowFill = PatternFill('solid', openpyxl.styles.colors.Color(rgb="FFFF66"))
     prt_curr_row += 1 # skip 1 row
     p_ws.cell(prt_curr_row,1).value="Networth"
-    print(prt_mf_tot_row)
-    print(prt_bank_tot_row)
     p_ws.cell(prt_curr_row,2).value= '=SUM($F${0},$C${1})'.format(prt_mf_tot_row,prt_bank_tot_row)   # Formula to calculate Networth
     for col in range(1,3):
         p_ws.cell(prt_curr_row,col).fill = yellowFill
@@ -97,123 +117,136 @@ def create_networth():
     prt_curr_row += 1
 
 
-def create_bank_header():
-    global prt_curr_row
-    yellowFill = PatternFill('solid', openpyxl.styles.colors.Color(rgb="FFFF66"))
-    prt_curr_row += 1 # skip 1 row
-    p_ws.cell(prt_curr_row,1).value="Bank"
-    p_ws.cell(prt_curr_row,2).value="A/C"
-    p_ws.cell(prt_curr_row,3).value="Amt"
-    # Change the format for Mutual Fund header
-    for col in range(1,4):
-        p_ws.cell(prt_curr_row,col).fill = yellowFill
-        p_ws.cell(prt_curr_row,col).font = Font(bold=True)
-    prt_curr_row += 1
-
-
-def create_bank_detail():
+def write_bank_detail():
     global prt_curr_row, prt_bank_tot_row
-    p_ws.cell(prt_curr_row,1).value="ICICI"
-    p_ws.cell(prt_curr_row,2).value="XXXXXXXXXX"
-    p_ws.cell(prt_curr_row,3).value=0
+    add_blank_space()
+    write_header(heading_lst=["Bank","A/C","Amt"])
+    bank_detail_list=[]
+    if prt_owner=="Dilip":
+        bank_detail_list = dilip_bk_ac
+    elif prt_owner=="Sobha":
+        bank_detail_list = sobha_bk_ac
+    elif prt_owner=="Ratna":
+        bank_detail_list = ratna_bk_ac
+    elif prt_owner=="Brijesh":
+        bank_detail_list = brijesh_bk_ac
+    bank_detail_start_row = prt_curr_row
+    bank_detail_end_row = prt_curr_row + len(bank_detail_list)-1
+    for item in bank_detail_list:
+        p_ws.cell(prt_curr_row,1).value=item["Bank"]
+        p_ws.cell(prt_curr_row,2).value=item["A/C"]
+        p_ws.cell(prt_curr_row,3).value=item["Amount"]
+        mark_input_cell(prt_curr_row,3)
+        prt_curr_row += 1
+    p_ws.cell(prt_curr_row,1).value="Bank Total"
+    p_ws.cell(prt_curr_row,2).value=""
+    p_ws.cell(prt_curr_row,3).value='=SUM($C${0}:$C${1})'.format(bank_detail_start_row,bank_detail_end_row)  # add bank total formula here
+
+    make_row_bold(prt_curr_row)
+
     prt_bank_tot_row = prt_curr_row
     prt_curr_row += 1
 
-def create_bank_summary():
-    pass
 
-def create_mf_summary():
+def write_mf_detail(mf_detail_lst):
     global prt_curr_row, prt_mf_tot_row
-    prt_curr_row +=1
-    p_ws.cell(prt_curr_row, 1).value = "Overall Mutual Fund"
-    p_ws.cell(prt_curr_row, 5).value = ws.cell(vr_mf_end_row+1, 8).value # Total Cost
-    p_ws.cell(prt_curr_row, 6).value = ws.cell(vr_mf_end_row+1, 10).value # Current Val
-    p_ws.cell(prt_curr_row, 9).value = ws.cell(vr_mf_end_row+1, 14).value # CAGR
-    prt_mf_tot_row = prt_curr_row
-    for col in range(1,11):
-        p_ws.cell(prt_curr_row,col).font = Font(bold=True)
-    prt_curr_row += 1
-
-def create_mf_detail():
-    global vr_mf_start_row,vr_mf_end_row,prt_curr_row
-    for row in range(vr_mf_start_row,vr_mf_end_row):
-        if ws.cell(row, 11).value > 1:     # add only if Units > 1
-            for col in range(1,15):
-                if col==1:
-                    p_ws.cell(prt_curr_row, 1).value = ws.cell(row, col).value  # Fund Name
-                elif col==2:
-                    p_ws.cell(prt_curr_row, 2).value = ws.cell(row, col).value  # Folio
-                elif col == 11:
-                    p_ws.cell(prt_curr_row, 3).value = ws.cell(row, col).value  # Units
-                elif col == 9:
-                    p_ws.cell(prt_curr_row, 4).value = ws.cell(row, col).value  # Cost per Units
-                elif col == 8:
-                    p_ws.cell(prt_curr_row, 5).value = ws.cell(row, col).value  # Total Cost
-                elif col==10:
-                    p_ws.cell(prt_curr_row, 6).value = ws.cell(row, col).value # Current Val
-                elif col == 13:
-                    p_ws.cell(prt_curr_row, 7).value = ws.cell(row, col).value  # Total Return
-                elif col == 5:
-                    p_ws.cell(prt_curr_row, 8).value = ws.cell(row, col).value  # NAV
-                elif col == 14:
-                    p_ws.cell(prt_curr_row, 9).value = ws.cell(row, col).value  # CAGR
-                elif col == 3:
-                    p_ws.cell(prt_curr_row, 10).value = ws.cell(row, col).value  # VR Rating
-                col +=1
-        row +=1
-        if ws.cell(row, 11).value > 1:
-            prt_curr_row +=1
+    add_blank_space()
+    write_header(heading_lst=["Fund Name","Folio","Units","Cost/Unit", "Tot Cost", "Curr Val","Tot Return", "NAV","CAGR","VR"])
+    for mf_detail in mf_detail_lst:
+        p_ws.cell(prt_curr_row, 1).value = mf_detail["Name"]
+        p_ws.cell(prt_curr_row, 2).value = mf_detail["Folio"]
+        p_ws.cell(prt_curr_row, 3).value = mf_detail["Units"]
+        p_ws.cell(prt_curr_row, 4).value = mf_detail["Cost_Unit"]
+        p_ws.cell(prt_curr_row, 5).value = mf_detail["Tot_Cost"]
+        p_ws.cell(prt_curr_row, 6).value = mf_detail["Curr_Val"]
+        p_ws.cell(prt_curr_row, 7).value = mf_detail["Tot_Return"]
+        p_ws.cell(prt_curr_row, 8).value = mf_detail["NAV"]
+        p_ws.cell(prt_curr_row, 9).value = mf_detail["CAGR"]
+        p_ws.cell(prt_curr_row, 10).value = mf_detail["VR"]
+        prt_curr_row +=1
+    prt_mf_tot_row = prt_curr_row-1
+    make_row_bold(prt_mf_tot_row)
 
 
-# create portfolio header
-def create_portfolio_header():
+# write portfolio header
+def write_portfolio_header():
     global prt_curr_row
-    p_ws.cell(prt_curr_row,1).value="Porfolio Owner"
-    p_ws.cell(prt_curr_row,2).value="Dilip Deb"
+    p_ws.cell(prt_curr_row,1).value="Portfolio Owner"
+    p_ws.cell(prt_curr_row,2).value=prt_owner
     p_ws.cell(prt_curr_row,3).value="Period"
-    p_ws.cell(prt_curr_row,4).value="Q1,FY22"
+    p_ws.cell(prt_curr_row,4).value=""
     p_ws.cell(prt_curr_row,5).value="Compiled On"
     p_ws.cell(prt_curr_row,6).value=str(datetime.date.today())
+    mark_input_cell(prt_curr_row,4)
+    make_row_bold(1)
     prt_curr_row += 1
 
-# create mutual fund header
-def create_mf_header():
+
+def write_header(heading_lst):
     global prt_curr_row
     yellowFill = PatternFill('solid', openpyxl.styles.colors.Color(rgb="FFFF66"))
-    prt_curr_row += 1 # skip 1 row
-    p_ws.cell(prt_curr_row,1).value="Mutual Fund"
-    p_ws.cell(prt_curr_row,2).value="Folio"
-    p_ws.cell(prt_curr_row,3).value="Units"
-    p_ws.cell(prt_curr_row,4).value="Cost/Unit"
-    p_ws.cell(prt_curr_row,5).value="Tot Cost"
-    p_ws.cell(prt_curr_row,6).value="Curr Val"
-    p_ws.cell(prt_curr_row,7).value="Tot Return"
-    p_ws.cell(prt_curr_row,8).value="NAV"
-    p_ws.cell(prt_curr_row,9).value="CAGR"
-    p_ws.cell(prt_curr_row,10).value="VR"
-
-    # Change the format for Mutual Fund header
-    for col in range(1,11):
-        p_ws.cell(prt_curr_row,col).fill = yellowFill
-        p_ws.cell(prt_curr_row,col).font = Font(bold=True)
-
+    heading_index=1
+    for heading in heading_lst:
+        p_ws.cell(prt_curr_row, heading_index).value = heading
+        p_ws.cell(prt_curr_row,heading_index).fill = yellowFill
+        p_ws.cell(prt_curr_row,heading_index).font = Font(bold=True)
+        heading_index +=1
     prt_curr_row += 1
+
+def mark_input_cell(row,col):
+    blueFill = PatternFill('solid', openpyxl.styles.colors.Color(rgb="99FFFF"))
+    p_ws.cell(row, col).fill = blueFill
+
+def make_row_bold(row):
+    for cell in p_ws['${0}:{1}'.format(row,row)]:
+        cell.font = Font(bold=True)
+
+def extract_mf_detail(vr_mf_start_row,vr_mf_end_row):
+    mf_detail_lst=[]
+    for row in range(vr_mf_start_row,vr_mf_end_row):
+        mf_detail = {}
+        if float(ws.cell(row,11).value) >1:
+            mf_detail["Name"]=ws.cell(row,1).value
+            mf_detail["Folio"]=ws.cell(row,2).value
+            mf_detail["Units"]=ws.cell(row,11).value
+            mf_detail["Cost_Unit"]=ws.cell(row,9).value
+            mf_detail["Tot_Cost"]=ws.cell(row,8).value
+            mf_detail["Curr_Val"]=ws.cell(row,10).value
+            mf_detail["Tot_Return"]=ws.cell(row,13).value
+            mf_detail["NAV"]=ws.cell(row,5).value
+            mf_detail["CAGR"]=ws.cell(row,14).value
+            mf_detail["VR"]=ws.cell(row,3).value
+            mf_detail_lst.append(mf_detail)
+    #add MF total row
+    mf_detail = {}
+    row = vr_mf_end_row+1
+    mf_detail["Name"] = ws.cell(row, 1).value
+    mf_detail["Folio"] = ws.cell(row, 2).value
+    mf_detail["Units"] = ws.cell(row, 11).value
+    mf_detail["Cost_Unit"] = ws.cell(row, 9).value
+    mf_detail["Tot_Cost"] = ws.cell(row, 8).value
+    mf_detail["Curr_Val"] = ws.cell(row, 10).value
+    mf_detail["Tot_Return"] = ws.cell(row, 13).value
+    mf_detail["NAV"] = ws.cell(row, 5).value
+    mf_detail["CAGR"] = ws.cell(row, 14).value
+    mf_detail["VR"] = ws.cell(row, 3).value
+    mf_detail_lst.append(mf_detail)
+
+    return mf_detail_lst
+
 
 # Get co-ordinates of details in VR File
 def get_vr_cordinate():
-    # While global variables can be accessed from functions, to change their value use "global" keyword
-    global vr_curr_row, vr_mf_start_row, vr_mf_end_row, vr_mf_cost,vr_mf_value,vr_mf_tot_return,vr_mf_cagr
+    global vr_curr_row, vr_mf_start_row, vr_mf_end_row,prt_owner
     for row in ws.rows:
-        if row[0].value == "Fund Name":
+        if str(row[0].value).strip() == "Fund Name":
             vr_mf_start_row=vr_curr_row + 1
-        elif row[0].value == "Mutual Funds":
+        elif str(row[0].value).strip() == "Mutual Funds":
             vr_mf_end_row = vr_curr_row-1
-            vr_mf_cost=row[7].value
-            vr_mf_value=row[9].value
-            vr_mf_tot_return=row[12].value
-            vr_mf_cagr=row[13].value
+        elif str(row[0].value).strip() == "Selected labels":
+            prt_owner=row[2].value
+
         vr_curr_row += 1
 
 
 create_portfolio()
-
